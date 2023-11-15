8f4d2c5af5219e83327c97ab7a0219ea1f2417e5
anthony morand <anthonymorand77@gmail.com>
2023-11-15 12:29:38 +0100

    ts-redux courses

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { store } from "../state";
+import { Provider } from "react-redux";
+import RepositoriesList from './RepositoriesList';
+
+const App = () => {
+  return <Provider store={store}>
+    <div>
+      <h1>Search for a repositories</h1>
+      <RepositoriesList />
+    </div> 
+  </Provider>
+};
+
+export default App;
